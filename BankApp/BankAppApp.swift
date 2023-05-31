43222f821b570fcb1538d948e200f9007244596a
Aleksey <oldf03@gmail.com>
2023-05-31 13:34:41 +0300

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Buba on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
